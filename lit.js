dc5a31ce2c5888af6ba36786b8a2cee664dbf5d3
fortv8843 <157026782+fortv8843@users.noreply.github.com>
2024-01-18 13:17:05 -0500

    remove elements, replace date with now

--- lit.js
+++ lit.js
@@ -1,27 +1,22 @@
 import {html, LitElement, render} from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
 
 export class TimeLeft extends LitElement {
-  static properties = {
-    date: {type: Date},
-    times: {type: [Date]},
-  };
-
   time(hour, minute) {
-    return new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate(), hour, minute)
+    return new Date(this.now.getFullYear(), this.now.getMonth(), this.now.getDate(), hour, minute)
   }
   
   constructor() {
     super();
     
     setInterval(() => {
-      this.date = new Date();
+      this.now = new Date();
       this.times = [this.time(9, 30), this.time(10, 50), this.time(13, 5), this.time(14, 25)]
     }, 100);
   }
 
   timeUntil(time) {
-    const totalCurrentMinutes = this.date.getHours() * 60 + this.date.getMinutes()
-    const totalCurrentSeconds = totalCurrentMinutes * 60 + this.date.getSeconds()
+    const totalCurrentMinutes = this.now.getHours() * 60 + this.now.getMinutes()
+    const totalCurrentSeconds = totalCurrentMinutes * 60 + this.now.getSeconds()
     const totalTimeMinutes =
       time.getHours() * 60 + time.getMinutes()
     const totalTimeSeconds = totalTimeMinutes * 60
@@ -37,7 +32,7 @@ export class TimeLeft extends LitElement {
 
   render() {
     for (let time of this.times) {
-      if (this.date > time) continue
+      if (this.now > time) continue
       return html`${this.timeUntil(time)}`
     }
   }
