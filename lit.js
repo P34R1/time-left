dafad04ae26d1e72234e09fb7d49d9f1df9ecbef
fortv8843 <157026782+fortv8843@users.noreply.github.com>
2024-01-18 13:45:00 -0500

    styles and format

--- lit.js
+++ lit.js
@@ -1,41 +1,80 @@
-import {html, LitElement, render} from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
-
-export class TimeLeft extends LitElement {
-  time(hour, minute) {
-    return new Date(this.now.getFullYear(), this.now.getMonth(), this.now.getDate(), hour, minute)
-  }
+import {
+    html,
+    LitElement,
+    css,
+  } from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js"
   
-  constructor() {
-    super();
-    
-    setInterval(() => {
-      this.now = new Date();
-      this.times = [this.time(9, 30), this.time(10, 50), this.time(13, 5), this.time(14, 25)]
-    }, 100);
-  }
+  export class TimeLeft extends LitElement {
+    static styles = css`
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+        height: 100vh;
+        background: #AFBBF2;
+        color: #000;
+      }
+      
+      time-left {
+        background: #D3FFF3;
+        padding: 100px;
+        font-size: 90px;
+        border-radius: 25px;
+        border: 2px solid;
+        box-shadow: 5px 10px 8px #000;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    `
 
-  timeUntil(time) {
-    const totalCurrentMinutes = this.now.getHours() * 60 + this.now.getMinutes()
-    const totalCurrentSeconds = totalCurrentMinutes * 60 + this.now.getSeconds()
-    const totalTimeMinutes =
-      time.getHours() * 60 + time.getMinutes()
-    const totalTimeSeconds = totalTimeMinutes * 60
-
-    const totalSecondsLeft = totalTimeSeconds - totalCurrentSeconds
-    const minutes = Math.floor(totalSecondsLeft / 60)
-      .toString()
-      .padStart(2, "0")
-    const seconds = (totalSecondsLeft % 60).toString().padStart(2, "0")
-
-    return `${minutes}m ${seconds}s`
-  }
-
-  render() {
-    for (let time of this.times) {
-      if (this.now > time) continue
-      return html`${this.timeUntil(time)}`
+    time(hour, minute) {
+      return new Date(
+        this.now.getFullYear(),
+        this.now.getMonth(),
+        this.now.getDate(),
+        hour,
+        minute,
+      )
+    }
+  
+    constructor() {
+      super()
+  
+      setInterval(() => {
+        this.now = new Date()
+        this.times = [
+          this.time(9, 30),
+          this.time(10, 50),
+          this.time(13, 5),
+          this.time(14, 25),
+        ]
+      }, 100)
+    }
+  
+    timeUntil(time) {
+      const totalCurrentMinutes = this.now.getHours() * 60 + this.now.getMinutes()
+      const totalCurrentSeconds = totalCurrentMinutes * 60 + this.now.getSeconds()
+      const totalTimeMinutes = time.getHours() * 60 + time.getMinutes()
+      const totalTimeSeconds = totalTimeMinutes * 60
+  
+      const totalSecondsLeft = totalTimeSeconds - totalCurrentSeconds
+      const minutes = Math.floor(totalSecondsLeft / 60)
+        .toString()
+        .padStart(2, "0")
+      const seconds = (totalSecondsLeft % 60).toString().padStart(2, "0")
+  
+      return `${minutes}m ${seconds}s`
+    }
+  
+    render() {
+      for (let time of this.times) {
+        if (this.now > time) continue
+        return html`${this.timeUntil(time)}`
+      }
     }
   }
-}
-
-customElements.define('time-left', TimeLeft);
+  
+  customElements.define("time-left", TimeLeft)
+  
